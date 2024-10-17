acc2ff7ca898c5c844924935addb90b32183d5d4
Dani <hohoho.GG81@gmail.com>
2024-10-18 01:58:34 +0200

    Deleted unfinished stuff

--- engine.py
+++ engine.py
@@ -19,29 +19,6 @@ def simplify_list(nested_list):
   else:
     return nested_list
 
-def simplify_outer_layers(input_list):
-    # Initialize a list to hold the simplified result
-    simplified_list = []
-    
-    # Recursive function to flatten the outer layers
-    def flatten_outer(lst):
-        # If the current list is a list of exactly two lists, return it as is
-        if isinstance(lst, list) and len(lst) == 2 and all(isinstance(i, list) for i in lst):
-            return [lst]
-        # Otherwise, flatten the outer layers
-        flattened = []
-        for item in lst:
-            if isinstance(item, list):
-                flattened.extend(flatten_outer(item))  # Recursively flatten outer lists
-            else:
-                flattened.append(item)
-        return flattened
-
-    # Simplify the entire input list
-    simplified_list = flatten_outer(input_list)
-    
-    return simplified_list
-
 def simplify_nested_list(nested_list):
     # Continue simplifying while the list contains exactly one list
     while isinstance(nested_list, list) and len(nested_list) == 1 and isinstance(nested_list[0], list):
@@ -158,28 +135,6 @@ def organize_list(lst1, lst2):
         index += size
     return result
 
-def make_tree(branches,positions):
-    tree = [branches[0]]
-    position_tree = [positions[0]]
-    for i in range(len(branches)-1):
-        if i == 0:
-            tree.append(organize_list(branches[i+1],tree[i]))
-            position_tree.append(organize_list(positions[i+1],tree[i]))
-        else:
-            if isinstance(simplify_list(tree[i])[0],int):
-                tree.append(organize_list(branches[i+1],simplify_list(tree[i])))
-                position_tree.append(organize_list(positions[i+1],simplify_list(tree[i])))
-            else:
-                tree.append(organize_list(
-                    branches[i+1],
-                    [sum(lst) for lst in simplify_list(tree[i])]
-                ))
-                position_tree.append(organize_list(
-                    positions[i+1],
-                    [sum(lst) for lst in simplify_list(tree[i])]
-                ))
-    return tree, position_tree
-
 def find_sublist_index(nested_list, target_list):
     for idx, sublist in enumerate(nested_list):
         if sublist == target_list:
@@ -292,129 +247,6 @@ def find_best_move(positions,branches,sheep_to_move):
 
     return positions[1][posibilities[0].index(puntuation[0])]
 
-def find_best_move_2(positions,branches,sheep_to_move):
-    num_iterations = len(positions)-1
-    if num_iterations % 2 == 0:
-        if sheep_to_move:
-            sheep_to_move = False
-        else:
-            sheep_to_move = True
-    puntuations = all_puntuations(positions[-1],branches[-1])
-    print(puntuations)
-    if sheep_to_move:
-        puntuation = [max(move) for move in puntuations if len(move) != 0]
-    else:
-        puntuation = [min(move) for move in puntuations if len(move) != 0]
-    for branch in reversed(branches):
-        if sheep_to_move:
-            posibilities = organize_list(puntuation,branch)
-            puntuation = [max(move) for move in posibilities if len(move)!=0]
-            sheep_to_move = False
-        else:
-            # print(len(puntuation))
-            print(sum([sum(x) for x in branch]))
-            posibilities = organize_list(puntuation,branch)
-            puntuation = [min(move) for move in posibilities if len(move)!=0]
-            sheep_to_move = True
-
-    return positions[1][posibilities[0].index(puntuation[0])]
-
-def all_puntuations(positions,branches):
-    puntuations = []
-    for position in positions:
-        if all(not isinstance(i, list) for i in position[0]):
-            puntuations.append(puntuacion(position[0]))
-        else:
-            puntuations.append(all_puntuations(position))
-    puntuations = organize_list(puntuations,branches)
-    return puntuations
-
 def output_best_move(present,turn_sheep,depth=4):
     positions,branches = run_iterations(present,depth,turn_sheep)
     return find_best_move(positions,branches,turn_sheep)
-
-def run_iterations_organized(present,depth,sheep_to_move):
-    results_pos,n_outputs = [present],[]
-    organized_results,branches = [],[]
-    # results_points = [[puntuacion(present[0])]]
-    for i in range(depth):
-        t1 = time.time()
-        if i == 0:
-            # pos,poi,nou = neural_processor(results_pos[i],sheep_to_move,True)
-            pos,nou = neural_processor(results_pos[i],sheep_to_move,True)
-            results_pos.append(pos)
-            # results_points.append(poi)
-            n_outputs.append(nou)
-        else:
-            # pos,poi,nou = neural_processor(results_pos[i],sheep_to_move)
-            pos,nou = neural_processor(results_pos[i],sheep_to_move)
-            results_pos.append(pos)
-            # results_points.append(poi)
-            n_outputs.append(nou)
-        organized_results.append(organize_list(pos,nou))
-        branches.append(nou)
-        if sheep_to_move:
-            sheep_to_move = False
-        else:
-            sheep_to_move = True
-        # print(i,time.time()-t1)
-    # return results_pos,results_points,n_outputs
-    # organized_branches = [branches[0]]
-    # for i in range((len(branches))-1):
-    #     organized_branches.append(organize_list(branches[i+1],branches[i]))
-    tree_branches, tree_values = make_tree(branches,organized_results)
-    return tree_values, tree_branches
-
-def new_iteration(positions,move_made,branches,turn_sheep):
-    index = simplify_list(positions[0]).index(move_made)
-    new_tree,num_branches = [move_made],[]
-    for future_move in positions[1:]:
-        new_tree.append(simplify_list(future_move)[index])
-    new_moves,new_branches = [],[]
-    for branch in simplify_list(future_move)[index]:
-        f,n = neural_processor(branch,turn_sheep)
-        new_moves.append(f)
-        new_branches.append(n)
-    for branch in simplify_list(branches):
-        num_branches.append(branch[index])
-    new_tree.append(new_moves)
-    num_branches.append(new_branches)
-    return new_tree, num_branches
-
-def save_list_as_array_joblib(sheep_to_move,depth=6):
-    # Convert the list of lists into a NumPy array
-    present = [['A1','B1','C1','D1','E1','F1','G1','H1'],['D8']]
-    data_list,branches = run_iterations_organized(present,depth,sheep_to_move)
-    if sheep_to_move:
-        name1 = 'sheep_ite_tree_4.joblib' #Change to normal later
-        name2 = 'branches_sheep_ite_tree_4.joblib'
-    else:
-        name1 = 'wolf_ite_tree_4.joblib' #Change to normal later
-        name2 = 'branches_wolf_ite_tree_4.joblib'
-    filename1 = os.path.join(os.getcwd(),'First_iterations',name1) 
-    filename2 = os.path.join(os.getcwd(),'First_iterations',name2) 
-    numpy_array1 = np.array(data_list)
-    numpy_array2 = np.array(branches)
-
-    # Save the NumPy array using Joblib with optional compression (e.g., compress=3)
-    dump(numpy_array1, filename1, compress=3)  # compress=3 is a good balance between speed and file size
-    dump(numpy_array2, filename2, compress=3)
-    # print(f"Array successfully saved to {filename1}")
-
-# Function to load the saved array back from the Joblib file
-def load_array_from_joblib(filename):
-    # Load the NumPy array from the Joblib file
-    numpy_array = load(filename)
-    # print(f"Array successfully loaded from {filename}")
-    return numpy_array
-
-# save_list_as_array_joblib(True)
-# save_list_as_array_joblib(False)
-
-# save_list_as_array_joblib(True,4)
-# save_list_as_array_joblib(False,4)
-
-positions = load_array_from_joblib(r"C:\Users\34673\Documents\Python\chess\First_iterations\wolf_ite_tree_4.joblib").tolist()
-branches = load_array_from_joblib(r"C:\Users\34673\Documents\Python\chess\First_iterations\branches_wolf_ite_tree_4.joblib").tolist()
-
-present = [['A1','B1','C1','E1','F1','G1','H1'],['D1']]
